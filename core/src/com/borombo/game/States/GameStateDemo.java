9b84857671beba3b2eeaf508e0f541eb5f3abc76
Borombo <e.rombo@yahoo.com>
2016-03-19 20:08:19 +0100

    Ajout du Timer

--- core/src/com/borombo/game/States/GameStateDemo.java
+++ core/src/com/borombo/game/States/GameStateDemo.java
@@ -1,27 +1,50 @@
 package com.borombo.game.States;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
 import com.borombo.game.ClickMoreDemo;
 import com.borombo.game.Sprites.RedBackground;
 import com.borombo.game.Sprites.SimpleCircle;
 
+import java.text.NumberFormat;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 /**
  * Created by Erwan on 21/02/2016.
  */
 public class GameStateDemo extends State {
-    private Texture background;
     private SimpleCircle circle;
     private RedBackground rbg;
+    private int score = 0;
+    private boolean inGame;
+    NumberFormat numberFormat = NumberFormat.getInstance();
+    private float restTime = 60.00f;
+    private BitmapFont bfScore;
+    private BitmapFont bfTime;
+    private Timer timer = new Timer();
 
     public GameStateDemo(GameStateManager gsm) {
         super(gsm);
         rbg = new RedBackground();
         Vector3 pos = new Vector3(100, 610, 0);
         circle = new SimpleCircle(pos);
+        inGame = true;
+        bfScore = new BitmapFont();
+        bfTime = new BitmapFont();
+        numberFormat.setMinimumFractionDigits(1);
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+              if (restTime > 0.1)
+                restTime-=0.1;
+              else
+                  inGame = false;
+            }
+        }, 100, 100);
     }
 
     @Override
@@ -32,6 +55,7 @@ public class GameStateDemo extends State {
             if (input.x > circle.getPosition().x+40 && input.x < circle.getPosition().x+circle.getSize().x-20 &&
                     input.y > circle.getPosition().y+40 && input.y < circle.getPosition().y+circle.getSize().y){
                 this.circle.touched();
+                score++;
             }
             System.out.println("Click Position x : " + input.x);
             System.out.println("Click Position y : " + input.y);
@@ -40,8 +64,10 @@ public class GameStateDemo extends State {
 
     @Override
     public void update(float dt) {
-        handleInput();
-        circle.update(dt);
+        if (inGame) {
+            handleInput();
+            circle.update(dt);
+        }
     }
 
     @Override
@@ -49,6 +75,8 @@ public class GameStateDemo extends State {
         sb.begin();
         sb.draw(rbg.getTexture(), 0, 0, ClickMoreDemo.WHIDTH, ClickMoreDemo.HEIGHT);
         sb.draw(circle.getRegionCourante(), circle.getPosition().x, circle.getPosition().y);
+        bfScore.draw(sb,"Score : " + String.valueOf(score),390, 730);
+        bfTime.draw(sb,"Time : " + numberFormat.format(restTime),10,730);
         sb.end();
     }
 }
